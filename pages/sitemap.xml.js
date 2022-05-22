172bfbbb648283f0aa8b8e7d45493db3ee79dbc2
Princenick-BL <princenick@gmail.com>
2022-05-22 02:55:15 +0200

    updates

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,61 @@
+import React from "react";
+import fs from "fs";
+
+const Sitemap = () => {};
+
+export const getServerSideProps = ({ res }) => {
+  const baseUrl = {
+    development: "http://localhost:5000",
+    production: "https://mydomain.com",
+  }[process.env.NODE_ENV];
+
+  const staticPages = fs
+    .readdirSync("pages")
+    .filter((staticPage) => {
+      return ![
+        "_app.js",
+        "_document.js",
+        "_error.js",
+        "sitemap.xml.js",
+      ].includes(staticPage);
+    })
+    .map((staticPagePath) => {
+      return `${baseUrl}/${staticPagePath}`;
+    });
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset
+            xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+            xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+                http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+    <!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
+    
+    
+    <url>
+        <loc>https://www.discoverai.app/</loc>
+        <lastmod>2022-05-22T00:38:05+00:00</lastmod>
+    </url>
+    <url>
+        <loc>https://www.discoverai.app/contact-us</loc>
+        <lastmod>2022-05-22T00:38:05+00:00</lastmod>
+    </url>
+    <url>
+        <loc>https://www.discoverai.app/tool/design/gradient-generator</loc>
+        <lastmod>2022-05-22T00:38:05+00:00</lastmod>
+    </url>
+    
+    
+    </urlset>
+  `;
+
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+};
+
+export default Sitemap;
