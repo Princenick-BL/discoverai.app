bc2900b51e720dda9ad2ad08a8c634d2318a00ac
Princenick-BL <princenick@gmail.com>
2022-05-28 23:59:46 +0200

    updates

--- pages/sitemap_index.xml/index.js
+++ pages/sitemap_index.xml/index.js
@@ -9,29 +9,25 @@ export const getServerSideProps = ({ res }) => {
   }[process.env.NODE_ENV];
 
   
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset
-            xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-            xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-                http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-    <!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
-    
-    
-    <url>
+  const sitemap = `
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+    <!--  created with Free Online Sitemap Generator www.xml-sitemaps.com  -->
+      <url>
         <loc>https://www.discoverai.app/</loc>
-        <lastmod>2022-05-22T00:38:05+00:00</lastmod>
-    </url>
-    <url>
-        <loc>https://www.discoverai.app/contact-us</loc>
-        <lastmod>2022-05-22T00:38:05+00:00</lastmod>
-    </url>
-    <url>
-        <loc>https://www.discoverai.app/tool/design/gradient-generator</loc>
-        <lastmod>2022-05-22T00:38:05+00:00</lastmod>
-    </url>
-    
-    
+        <lastmod>2022-05-28T21:18:56+00:00</lastmod>
+      </url>
+      <url>
+        <loc>https://www.discoverai.app/design/gradient-generator</loc>
+        <lastmod>2022-05-28T21:18:56+00:00</lastmod>
+      </url>
+      <url>
+        <loc>https://www.discoverai.app/ai/text-to-speach-voxai</loc>
+        <lastmod>2022-05-28T21:18:56+00:00</lastmod>
+      </url>
+      <url>
+        <loc>https://www.discoverai.app/tuto/sitemap.xml</loc>
+        <lastmod>2022-05-28T21:18:56+00:00</lastmod>
+      </url>
     </urlset>
   `;
 
--- pages/tuto/sitemap.xml/index.js
+++ pages/tuto/sitemap.xml/index.js
@@ -1,39 +1,44 @@
 import React from "react";
+import { getTuto, getTutos } from "../../../services/tutos";
 
 const Sitemap = () => {};
 
-export const getServerSideProps = ({ res }) => {
+export const getServerSideProps = async ({ res }) => {
+
   const baseUrl = {
-    development: "http://localhost:5000",
-    production: "https://www.discoverai.app",
+    development: "http://localhost:3000/tuto/",
+    production: "https://www.discoverai.app/tuto/",
   }[process.env.NODE_ENV];
 
-  
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  const tutuoRes = await getTutos("tuto");
+
+
+  var sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset
-            xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-            xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-                http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-    <!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
-    
-    
-    <url>
-        <loc>https://www.discoverai.app/</loc>
-        <lastmod>2022-05-22T00:38:05+00:00</lastmod>
-    </url>
-    <url>
-        <loc>https://www.discoverai.app/contact-us</loc>
-        <lastmod>2022-05-22T00:38:05+00:00</lastmod>
-    </url>
-    <url>
-        <loc>https://www.discoverai.app/tool/design/gradient-generator</loc>
-        <lastmod>2022-05-22T00:38:05+00:00</lastmod>
-    </url>
-    
-    
-    </urlset>
-  `;
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+      http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+      <!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
+      <url>
+        <loc>https://www.discoverai.app/tuto/nodejs-text-to-speech-api--4</loc>
+        <lastmod>2022-05-28T00:38:05+00:00</lastmod>
+      </url>
+      `;
+
+  console.log(tutuoRes)
+  if(tutuoRes?.succes){
+    tutuoRes?.data.map((tuto,index)=>{
+      return sitemap=sitemap+`
+        <url>
+            <loc>https://www.discoverai.app/tuto/nodejs-text-to-speech-api--4</loc>
+            <lastmod>2022-05-28T00:38:05+00:00</lastmod>
+        </url>`
+    })
+  }
+  
+  sitemap +="</urlset>"
+ 
 
   res.setHeader("Content-Type", "text/xml");
   res.write(sitemap);
