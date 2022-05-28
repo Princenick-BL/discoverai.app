9b14a081b70c01d3a085901cc569efafb69a7fdf
Princenick-BL <princenick@gmail.com>
2022-05-29 00:28:31 +0200

    updates

--- pages/sitemap_index.xml/index.js
+++ pages/sitemap_index.xml/index.js
@@ -9,41 +9,25 @@ export const getServerSideProps = ({ res }) => {
   }[process.env.NODE_ENV];
 
   
-  const sitemap = `
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset 
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd
+    http://www.w3.org/1999/xhtml http://www.w3.org/2002/08/xhtml/xhtml1-strict.xsd"
+    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+    >
     <!--  created with Free Online Sitemap Generator www.xml-sitemaps.com  -->
       <url>
         <loc>https://www.discoverai.app/</loc>
         <lastmod>2022-05-28T21:18:56+00:00</lastmod>
-        <xhtml:link rel="alternate" hreflang="en-fr" href="https://www.discoverai.app" />
-        <xhtml:link rel="alternate" hreflang="en-in" href="https://www.discoverai.app" />
-        <xhtml:link rel="alternate" hreflang="en-us" href="https://www.discoverai.app" />
-        <xhtml:link rel="alternate" hreflang="en" href="https://www.discoverai.app" />
-        <xhtml:link rel="alternate" hreflang="en-es" href="https://www.discoverai.app" />
-        <xhtml:link rel="alternate" hreflang="en-be" href="https://www.discoverai.app" />
-        <xhtml:link rel="alternate" hreflang="en-gb" href="https://www.discoverai.app" />
       </url>
       <url>
         <loc>https://www.discoverai.app/design/gradient-generator</loc>
         <lastmod>2022-05-28T21:18:56+00:00</lastmod>
-        <xhtml:link rel="alternate" hreflang="en-fr" href="https://www.discoverai.app/design/gradient-generator" />
-        <xhtml:link rel="alternate" hreflang="en-in" href="https://www.discoverai.app/design/gradient-generator" />
-        <xhtml:link rel="alternate" hreflang="en-us" href="https://www.discoverai.app/design/gradient-generator" />
-        <xhtml:link rel="alternate" hreflang="en" href="https://www.discoverai.app/design/gradient-generator" />
-        <xhtml:link rel="alternate" hreflang="en-es" href="https://www.discoverai.app/design/gradient-generator" />
-        <xhtml:link rel="alternate" hreflang="en-be" href="https://www.discoverai.app/design/gradient-generator" />
-        <xhtml:link rel="alternate" hreflang="en-gb" href="https://www.discoverai.app/design/gradient-generator" />
       </url>
       <url>
         <loc>https://www.discoverai.app/ai/text-to-speach-voxai</loc>
         <lastmod>2022-05-28T21:18:56+00:00</lastmod>
-        <xhtml:link rel="alternate" hreflang="en-fr" href="https://www.discoverai.app/ai/text-to-speach-voxai" />
-        <xhtml:link rel="alternate" hreflang="en-in" href="https://www.discoverai.app/ai/text-to-speach-voxai" />
-        <xhtml:link rel="alternate" hreflang="en-us" href="https://www.discoverai.app/ai/text-to-speach-voxai" />
-        <xhtml:link rel="alternate" hreflang="en" href="https://www.discoverai.app/ai/text-to-speach-voxai" />
-        <xhtml:link rel="alternate" hreflang="en-es" href="https://www.discoverai.app/ai/text-to-speach-voxai" />
-        <xhtml:link rel="alternate" hreflang="en-be" href="https://www.discoverai.app/ai/text-to-speach-voxai" />
-        <xhtml:link rel="alternate" hreflang="en-gb" href="https://www.discoverai.app/ai/text-to-speach-voxai" />
       </url>
       <url>
         <loc>https://www.discoverai.app/tuto/sitemap.xml</loc>
